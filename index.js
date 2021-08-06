8a1491e75b49fa88ca13e00960786596e1b01679
sandesh <sundesz@gmail.com>
2021-08-06 14:10:53 +0300

    Added frontend build directory

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
